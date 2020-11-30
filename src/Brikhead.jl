1aabe10923a75a69dc011a4ce42e6ecc786b58a0
Zaki A <zaki@live.ca>
2020-11-29 20:33:57 -0600

    Switch to CodecZlib; Add ability to write Brikhead files

--- src/Brikhead.jl
+++ src/Brikhead.jl
@@ -1,8 +1,9 @@
 module Brikhead
 
-using Libz
+using CodecZlib
 
 export load_brikhead, load_brikhead_dir
+export write_brikhead
 
 struct AFNIAttribute
     name
@@ -34,6 +35,7 @@ function get_chunks(file)
     typelines = contains.(lines, r"type\s*=")
     chunks = Array{String,1}[]
     idx = findall(typelines)
+    push!(idx, length(lines)+1)
     for i in 2:length(idx)
         push!(chunks, lines[idx[i-1]:idx[i]-1])
     end
@@ -59,6 +61,10 @@ function parse_type(chunk)
     end
     error("No valid attribute found while parsing header in chunk:\n$(chunk)")
 end
+
+type2attribute(::Type{String}) = "string-attribute"
+type2attribute(::Type{Int}) = "integer-attribute"
+type2attribute(::Type{Float64}) = "float-attribute"
 
 function parse_name(chunk)
     nameline = chunk[2]
@@ -93,7 +99,7 @@ function clean_chunk_contents(chunk)
     filter!(element -> !isempty(element), str_array)
     return str_array
 end
-        
+       
 function pack_attributes_as_dict(attributes::Vector{AFNIAttribute})
     dict = Dict{Symbol, AFNIAttribute}()
     for attribute in attributes
@@ -133,6 +139,46 @@ function load_brikhead(file)
     img = parse_afni_brik(brikhead[:brik], hdr)
     return (; hdr, img)
 end
+
+function write_brikhead(file, brik)
+    write_afni_head(file, brik)
+    write_afni_brik(file, brik)
+    return file
+end
+
+function write_afni_head(file, brik)
+    file *= ".HEAD"
+    rm(file; force = true)
+    open(file, "w") do io 
+        for attribute in sort(collect(keys(brik.hdr)))
+            write_chunk(io, brik.hdr[attribute])
+        end
+    end
+    return
+end
+
+function write_chunk(io, chunk::AFNIAttribute)
+    out = [
+        "type = $(type2attribute(chunk.type))\n",
+        "name = $(chunk.name)\n",
+        "count = $(chunk.count)\n",
+    ]
+    if chunk.type == String
+        contents = replace(chunk.contents, "\\n" => "\n")
+        push!(out, "'" * contents * "~")
+    else
+        for (idx, str) in enumerate(string.(chunk.contents))
+            push!(out, " $str")
+            if rem(idx, 5) == 0
+                push!(out, "\n")
+            end
+        end
+    end
+    write(io, join(out))
+    write(io, "\n\n")
+    return
+end
+
 
 function filename_without_ext(filepath)
     filename = basename(filepath)
@@ -189,10 +235,37 @@ function parse_afni_brik(file, hdr)
     vol_shape = get_shape(hdr)
     num_elements = prod(vol_shape)
     briktype = briktypes[hdr.brick_types[1]]
-    filebytes = is_gz(file) ? Libz.inflate(read(file)) : read(file)
+    iobuffer = open(file)
+    if is_gz(file)
+        iobuffer = GzipDecompressorStream(iobuffer)
+    end
+    filebytes = read(iobuffer)
+    close(iobuffer)
     # ToDo? Account for: d.byteorder_string
     image_volume = reshape(reinterpret(briktype, filebytes), vol_shape)
     return permutedims(image_volume, (2,1,3,4))
+end
+
+function write_afni_brik(file, brik)
+    file = file * ".BRIK.gz"
+    rm(file; force = true)
+    hdr = brik.hdr
+    @assert length(unique(hdr.brick_types)) == 1
+    vol_shape = get_shape(hdr)
+    num_elements = prod(vol_shape)
+    briktype = briktypes[hdr.brick_types[1]]
+    img = permutedims(brik.img, (2,1,3,4))
+    bytes = reinterpret(briktype, vec(img))
+    iobuffer = open(file, "w")
+    if is_gz(file)
+        stream = GzipCompressorStream(iobuffer)
+        write(stream, bytes)
+        close(stream)
+    else
+        write(iobuffer, bytes)
+    end
+    close(iobuffer)
+    return file
 end
 
 end
